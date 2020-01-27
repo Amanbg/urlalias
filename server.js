9e80935d0307dd7713baa2a7fba76bb0b05abd6f
Amanbg <amanbabu8@gmail.com>
2020-01-27 15:40:16 +0530

    changed server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT|| 3000
 const path = require('path')
 
 app.use(express.static(__dirname));
 
-app.get('home/', function (req, res) {
+app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => console.log(`Magic port ${port}!`))
+app.listen(PORT, () => console.log(`Magic PORT ${PORT}!`))
